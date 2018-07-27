472a0b99c258191648c2cc7d2e40cb773bbf2561
unknown <jabs@ilfusion.com>
2018-07-27 18:00:49 +0800

    Clients Page in Trainer and Gym Dashboard Update

--- wp-content/themes/Divi-child/accounts/trainers/clients-page.php
+++ wp-content/themes/Divi-child/accounts/trainers/clients-page.php
@@ -1,14 +1,14 @@
 <div class="main-content matchHeight">
-	<?php if(isset($_GET['edit'])):?>
-		<div class="current-status">
-			<div class="row">
-				<?php get_template_part( 'accounts/inc/edit-progress', 'page' ); ?>
-			</div>
+<?php if(isset($_GET['edit'])):?>
+	<div class="current-status">
+		<div class="row">
+			<?php get_template_part( 'accounts/inc/edit-progress', 'page' ); ?>
 		</div>
-	<?php
-	elseif(isset($_GET['add'])):
+	</div>
+<?php
+elseif(isset($_GET['add'])):
 		get_template_part( 'accounts/trainers/add-clients', 'page' );
-	else: ?>
+else: ?>
 	<div class="trainer-add-workout">
 		<a href="<?php echo home_url(); ?>/trainer/?data=clients&add=1">+ Add Client</a>
 	</div>
@@ -24,62 +24,61 @@
 	        </tr>
 	    </thead>
 	    <tbody>
-	       <!-- <tr>
-	            <td><img src="<?php echo get_stylesheet_directory_uri().'/accounts/images/client-1.png';?>"></td>
-	            <td>Client Name #1</td>
-	            <td>Fat Loss</td>
-	            <td>4 Days Ago</td>
-	            <td>
-	            	<div class="progress client-goals-percentage">
-						<div class="progress-bar" style="width: 100%;">
-							<span class="indicator"><small>100%</small></span>
+<?php
+	$clients = get_user_meta(wp_get_current_user()->ID, 'clients_of_trainer', true);
+	if(!empty($clients)):
+		foreach($clients as $client):
+			$user = User::find($client);
+			$currentUser = [
+				'stats' => $user->getStats(),
+				'photos' => $user->getPhotos()
+			];		
+			
+			$eachBPStats = getGoalPerc($currentUser['stats']); //Bodypart %s
+			$bppavg = getBPPercAvg($eachBPStats); //Bodypart %s Average
+			
+			$user_info = get_user_by('id', $client);
+			$purpose =  get_user_meta($client, 'sm_accomtext', true);
+			
+			$uPho = array();
+			$userPhoto = "";
+			if(!empty($currentUser['photos'])){
+				$uPho = $currentUser['photos'];
+				$countPho = count($uPho) - 1;
+				$latestPho = $uPho[$countPho];
+				$userPhoto = home_url() . '/sm-files/' . $latestPho['user_id'] . '/' . $latestPho['file'];
+			}			
+			if($user_info):
+?>
+			<tr>
+				<td>
+					<?php
+						if($userPhoto)
+							echo '<img src="'.$userPhoto.'"  class="img-responsive" style="max-width:50px;" />';
+						else
+							echo '<img src="'.get_stylesheet_directory_uri().'/accounts/images/client-1.png" />';
+					?>
+				</td>
+				<td><?php echo $user_info->first_name . ' ' . $user_info->last_name;  ?></td>
+				<td> <?php echo ($purpose) ? $purpose : "--"; ?> </td>
+				<td>-- Days Ago</td>
+				<td>
+					<div class="progress client-goals-percentage">
+						<div class="progress-bar" style="width: <?php echo $bppavg; ?>%;">
+							<span class="indicator"><small><?php echo $bppavg; ?>%</small></span>
 						</div>
 					</div>
-	            </td>
-	        </tr> -->
-	       <?php $clients = get_user_meta(wp_get_current_user()->ID, 'clients_of_trainer', true);
-				if(!empty($clients)):
-				foreach($clients as $client):
-					$user = User::find($client);
-					$currentUser = [
-						'stats' => $user->getStats()
-					];
-					/* echo "<pre>";
-					print_r();
-					echo "</pre>"; */
-					
-					$eachBPStats = getGoalPerc($currentUser['stats']); //Each Bodypart Stats
-					$bppavg = getBPPercAvg($eachBPStats);
-					
-					$user_info = get_user_by('id', $client);
-					$user_ava = get_avatar( $client, 50);
-					$purpose =  get_user_meta($client, 'sm_accomtext', true);					
-					if($user_info):
-			?>
-				<tr>
-					<td>
-						<?php echo $user_ava; ?>			
-					</td>
-					<td><?php echo $user_info->first_name . ' ' . $user_info->last_name;  ?></td>
-					<td> <?php echo ($purpose) ? $purpose : "--"; ?> </td>
-					<td>-- Days Ago</td>
-					<td>
-						<div class="progress client-goals-percentage">
-							<div class="progress-bar" style="width: <?php echo $bppavg; ?>%;">
-								<span class="indicator"><small><?php echo $bppavg; ?>%</small></span>
-							</div>
-						</div>
-					</td>
-					<td>
-						<a href="<?php echo home_url(); ?>/trainer/?data=clients&edit=<?php echo $user_info->ID; ?>">Edit Progress</a>
-					</td>
-				</tr>
-			<?php					
-					endif;
-				endforeach;
-				endif;
-			?>	
+				</td>
+				<td>
+					<a href="<?php echo home_url(); ?>/trainer/?data=clients&edit=<?php echo $user_info->ID; ?>">Edit Progress</a>
+				</td>
+			</tr>
+<?php					
+			endif;
+		endforeach;
+	endif;
+?>	
 	    </tbody>
 	</table>
-	<?php endif; ?>
+<?php endif; ?>
 </div>
