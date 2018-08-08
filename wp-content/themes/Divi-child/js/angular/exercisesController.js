7da86fce4ebc676328b051dde262350e497630db
unknown <jabs@ilfusion.com>
2018-08-08 14:39:25 +0800

    Exercises Page and controller

--- wp-content/themes/Divi-child/js/angular/exercisesController.js
+++ wp-content/themes/Divi-child/js/angular/exercisesController.js
@@ -3,14 +3,30 @@ app.controller('exercisesController', function($scope, $http, $filter) {
     $scope.currentUser = CURRENT_USER;
     $scope.exerciseTypes = EXERCISE_TYPES;
     $scope.workoutTemplate = ROOT_URL + '/wp-content/themes/Divi-child/partials/exercises.html';
-
+	$scope.root_url = ROOT_URL;
+	
     init();
 
     function init ()
     {
         console.log('-------exercisesController----------');
-        console.log($scope.exerciseTypes);
+        console.log($scope.exerciseTypes);		
     }
+	
+	$scope.modalClick = function(){
+		var vsrc = this.type.video;
+		if(vsrc != ""){
+			if(vsrc.indexOf('youtu.be') != -1){
+				vsrc = vsrc.replace('https://youtu.be/','');
+			}else{
+				vsrc = vsrc.split('v=')[1];
+			}
+		}
+		$('#myModal').modal('show');
+		$('#myModal iframe').attr('src', 'https://www.youtube.com/embed/'+vsrc+'?rel=0&autoplay=1');
+	}
+	
+	
 
     $scope.tasks = [];
     $scope.filtered = {};
