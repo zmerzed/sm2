14fe974b06028f103fa38376118a84a8c505fe5c
remz <zmerzed@gmail.com>
2018-09-13 14:46:42 +0800

    updates

--- wp-content/themes/Divi-child/js/angular/app.js
+++ wp-content/themes/Divi-child/js/angular/app.js
@@ -89,6 +89,10 @@ app.constant('global', {
               });
                */
         return arr;
+    },
+    isObject : function isObject(val) {
+        if (val === null) { return false;}
+        return ( (typeof val === 'function') || (typeof val === 'object') );
     }
 });
 app.filter('dataURLtoBlob', function() {
