bed7aefc0ac3a0443b7066aa264672f09e7a9347
emma <admin@VLDCs-Mac-mini.local>
2018-11-29 18:01:38 +0800

    side bar and edit program print

--- wp-content/themes/Divi-child/accounts/inc/side-account.php
+++ wp-content/themes/Divi-child/accounts/inc/side-account.php
@@ -4,7 +4,7 @@
 	$op_train = (in_array( 'trainer', $uinfo->roles ) || $pm_lvl == "trainer");
 	$op_gym = (in_array( 'gym', $uinfo->roles ) || $pm_lvl == "gym");
 	$op_client = (in_array( 'client', $uinfo->roles ));
-	
+
 	if($op_train){
 		$urole = "trainer";
 	}elseif($op_gym){
@@ -12,7 +12,7 @@
 	}elseif($op_client){
 		$urole = "client";
 	}
-	
+
 	$r_uri = $_SERVER['REQUEST_URI'];
 ?>
 <div class="col-12 col-md-4 col-lg-3 col-xl-3 mb-5 d-none d-sm-none d-md-block">
@@ -21,7 +21,7 @@
 		<ul>
 			<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>" menu-item="dashboard" class="<?php echo($r_uri == '/'.$urole.'/') ? "active" : ""; ?>">Dashboard</a></li>
 			<li class="has-sub-drop">
-				<a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=schedule" menu-item="schedule">Schedule</a>				
+				<a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=schedule" menu-item="schedule">Schedule</a>
 				<ul class="sub-menu" style="display:none;">
 					<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=schedule&by=weekly" menu-item="weekly">Weekly</a></li>
 					<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=schedule&by=monthly" menu-item="monthly">Monthly</a></li>
@@ -33,18 +33,18 @@
 				<?php endif; ?>
 				<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=clients" menu-item="clients">Clients</a></li>
 				<li><a class="<?php echo (strpos($r_uri, 'workouts')) ? "active" : ""; ?>" href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=workouts" menu-item="workouts">Programs</a></li>
-				<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=exercises" menu-item="exercises">Exercises</a></li>				
+				<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=exercises" menu-item="exercises">Exercises</a></li>
 			<?php endif; ?>
-			<li class="<?php echo ($op_client) ? 'has-sub-drop' : ''; ?>"><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=profile" menu-item="profile">Profile</a>					
-				<?php if($op_client): ?>			
+			<li class="<?php echo ($op_client) ? 'has-sub-drop' : ''; ?>"><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=profile" menu-item="profile">Profile</a>
+				<?php if($op_client): ?>
 					<ul class="sub-menu" style="display:none;">
 						<li><a href="<?php echo home_url(); ?>/<?php echo $urole; ?>/?data=profile&by=personal-info" menu-item="personal-info">Personal Info</a></li>
 						<li><a href="<?php echo home_url(); ?>/<?php echo $urole; ?>/?data=profile&by=progress-goals" menu-item="progress-goals">Progress/Goals</a></li>
-					</ul>				
-				<?php endif; ?>		
-			</li>			
-			<!--li><a class="<?php echo (strpos($r_uri, 'messages')) ? "active" : ""; ?>" href="<?php echo  home_url(); ?>/messages" menu-item="message">Messages</a></li>
-			<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=notes" menu-item="notes">Notes</a></li-->
+					</ul>
+				<?php endif; ?>
+			</li>
+			<li><a class="<?php echo (strpos($r_uri, 'messages')) ? "active" : ""; ?>" href="<?php echo  home_url(); ?>/messages" menu-item="message">Messages</a></li>
+			<!--li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=notes" menu-item="notes">Notes</a></li-->
 			<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=logs" menu-item="logs">Logs</a></li>
 			<?php if(checkSubscribed($uinfo)): ?>
 				<li class="has-sub-drop"><a href="<?php echo  home_url(); ?>/membership-account/" menu-item="member">Member Subscription</a>
@@ -59,9 +59,9 @@
 							<!-- <li><a href="<?php echo home_url();  ?>/membership-account/membership-levels/">Member Levels</a></li> -->
 						</ul>
 					<?php //endif; ?>
-				</li>					
+				</li>
 			<?php endif; ?>
-		</ul>		
+		</ul>
 		<?php if($op_train || $op_gym): ?>
 			<!--div class="menu-divider"></div>
 
@@ -72,7 +72,7 @@
 					<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=trainers" menu-item="trainers">Trainers</a></li>
 				<?php endif; ?>
 				<li><a href="<?php echo  home_url(); ?>/<?php echo $urole; ?>/?data=clients" menu-item="clients">Clients</a></li>
-			</ul-->		
+			</ul-->
 		<?php endif; ?>
 	</div>
 </div>
@@ -81,5 +81,5 @@
 		$(document).ready(function(){
 			$('#menu .mobile-menu').html($('.main-navigation').html());
 		});
-	})(jQuery);	
+	})(jQuery);
 </script>
